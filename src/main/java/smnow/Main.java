011b9306c4d61357779817465710c8e5a095635f
Yusuke Yamamoto <yusuke@mac.com>
2014-07-25 18:06:46 +0900

    dryRun を無効に

--- src/main/java/smnow/Main.java
+++ src/main/java/smnow/Main.java
@@ -34,7 +34,7 @@ public class Main {
     public static void main(String[] args) {
         Person[] persons = Person.load();
         Twitter twitter = TwitterFactory.getSingleton();
-        boolean dryRun = true;
+        boolean dryRun = false;
 
         Attendance attendance = new Attendance(new AttendanceListener() {
             @Override
